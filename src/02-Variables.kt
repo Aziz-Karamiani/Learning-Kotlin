ba08fc4dff88b9a231c767b0c22b4bb4120e6e29
a.karamiani <aziz.karamiani@gmail.com>
2023-02-17 16:48:21 +0330

    Variable in Kotlin

---
+++ src/02-Variables.kt
@@ -0,0 +1,13 @@
+package main
+
+fun main(args: Array<String>) {
+    var greeting = "Hello World!";
+    println(greeting);
+    val age = 32;
+    println(age);
+
+    greeting = "Hello World Again!";
+    println(greeting);
+
+    println("Done.");
+}
