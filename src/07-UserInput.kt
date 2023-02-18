42acad622427a3e890581e44b167466dc9c6f144
a.karamiani <aziz.karamiani@gmail.com>
2023-02-19 00:34:50 +0330

    User Input Convert to Int

--- src/07-UserInput.kt
+++ src/07-UserInput.kt
@@ -3,6 +3,9 @@ package main
 fun main(args: Array<String>) {
     println("What is your favourite programming language?")
     val userInput = readLine()
-
     println("Your favourite programming language is $userInput")
+
+    println("Enter a number...")
+    val yourNumber = readLine() ?: ""
+    println("Your number multiply with 2 is :  ${yourNumber.toInt() * 2}")
 }
