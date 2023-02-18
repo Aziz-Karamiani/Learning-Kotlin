e7dd5bbd976e8b96c8e82725dc201941621524e1
a.karamiani <aziz.karamiani@gmail.com>
2023-02-18 13:40:52 +0330

    Strings in Kotlin

---
+++ src/05-Strings.kt
@@ -0,0 +1,10 @@
+package main
+
+fun main(args: Array<String>) {
+    println("Hello Strings..")
+    // String with " and \ scape
+    println("The answer is \"yes\" and you can proceed.!")
+
+    // Chars in the '
+    println('A')
+}
