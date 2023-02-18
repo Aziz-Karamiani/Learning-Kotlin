c6c92d4cfa54be590065a9827474aee1a1ff99ce
a.karamiani <aziz.karamiani@gmail.com>
2023-02-19 00:30:11 +0330

    User Input as String

---
+++ src/07-UserInput.kt
@@ -0,0 +1,8 @@
+package main
+
+fun main(args: Array<String>) {
+    println("What is your favourite programming language?")
+    val userInput = readLine()
+
+    println("Your favourite programming language is $userInput")
+}
