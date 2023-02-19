f914fb2a2cc9bcf4781174562fd826809f7acfca
a.karamiani <aziz.karamiani@gmail.com>
2023-02-19 23:09:23 +0330

    Numbers get types

--- src/09-Numbers.kt
+++ src/09-Numbers.kt
@@ -6,4 +6,15 @@ fun main(args: Array<String>) {
     val totalPrice = total * price
     println("Total price is $totalPrice")
 
+    val age = 32
+    println(age::class.java)
+
+    val people = 8350000000
+    println(people::class.java)
+
+    val grade = 19.7
+    println(grade::class.java)
+
+    val pi = 3.1415678654
+    println(pi::class.java)
 }
