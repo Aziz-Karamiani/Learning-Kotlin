d9daa73b0ff96cac1e498dea9a9d47cb6a0ee304
a.karamiani <aziz.karamiani@gmail.com>
2023-02-20 19:39:33 +0330

    Operators and booleans

---
+++ src/11-Operators and Booleans.kt
@@ -0,0 +1,30 @@
+package main
+
+fun main(args: Array<String>) {
+    // Arithmetic
+    var a: Int = 5
+    val b: Int = 2
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+
+    println(a++)
+    println(++a)
+
+    println(a--)
+    println(--a)
+
+    a += 1
+    println(a)
+    a -= 1
+    println(a)
+    a *= 2
+    println(a)
+    a /= 2
+    println(a)
+    a %= 2
+    println(a)
+
+}
