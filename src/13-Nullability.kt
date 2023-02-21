fc76d0f9763f4dee888934828b1ec82957aa4358
a.karamiani <aziz.karamiani@gmail.com>
2023-02-21 08:25:34 +0330

    Null Arithmetic operators

--- src/13-Nullability.kt
+++ src/13-Nullability.kt
@@ -8,4 +8,11 @@ fun main(args: Array<String>) {
     val catName: String? = null
     println(catName)
     println(catName?.length)
+
+    val amount: Int? = 10
+    println(amount?.plus(10))
+    println(amount?.minus(5))
+    println(amount?.times(10))
+    println(amount?.div(2))
+    println(amount?.rem(3))
 }
