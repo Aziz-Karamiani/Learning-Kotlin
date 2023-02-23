4b98e1afd2d3919f797eb4ba33b40ef246b645f1
a.karamiani <aziz.karamiani@gmail.com>
2023-02-23 13:05:29 +0330

    HashSet and Set Functions Practice

---
+++ src/32- Set and HashSet Practice.kt
@@ -0,0 +1,16 @@
+package main
+
+fun main(args: Array<String>) {
+    /*
+        Your company has very strict dress code. Only certain color clothes can be worn in the office.
+        val acceptedColors = hashSetOf(“white”, “black”, “grey”)
+        You have certain colors in your wardrobe.
+        val myColors = hashSetOf(“blue”, “red”, “black”, “green”)
+        What color clothes can you wear.
+        Your company has added another color, “red” to their list. What options do you have now?
+     */
+    val acceptedColors = hashSetOf("white", "black", "grey")
+    val myColors = hashSetOf("blue", "red", "black", "green")
+    myColors.retainAll(acceptedColors)
+    println(myColors)
+}
