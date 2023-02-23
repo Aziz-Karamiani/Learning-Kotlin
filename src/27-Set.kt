5fea9df437788ece95d8f035381690db1a04b0a5
a.karamiani <aziz.karamiani@gmail.com>
2023-02-23 12:07:30 +0330

    Set

---
+++ src/27-Set.kt
@@ -0,0 +1,13 @@
+package main
+
+fun main(args: Array<String>) {
+    val numbers = setOf(1, 2, 3, 4, 5, 5, 5, 5)
+    println(numbers)
+
+    val numbers2 = setOf(1, 2, 3, 4, null, null)
+    println(numbers2)
+
+//    val numbers3 = setOf<Int>(1, 2, 3, 4, null, null)
+//    println(numbers3)
+
+}
