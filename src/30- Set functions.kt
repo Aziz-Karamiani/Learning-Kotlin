f9b238041106676dacb029b67c9392dc5e7960ef
a.karamiani <aziz.karamiani@gmail.com>
2023-02-23 12:54:51 +0330

    Set Functions

---
+++ src/30- Set functions.kt
@@ -0,0 +1,15 @@
+package main
+
+fun main(args: Array<String>) {
+    val numbers = setOf(1, 2, 3, 4, 5)
+    println(numbers.size)
+    println(numbers.contains(2))
+    println(numbers.contains(6))
+
+    val numbers2 = setOf(4, 5)
+    val numbers3 = setOf(5, 6)
+    println(numbers.containsAll(numbers2))
+    println(numbers.containsAll(numbers3))
+
+    println(numbers.isEmpty())
+}
