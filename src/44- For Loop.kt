64ee1322f633b13e656820395cb14298258ab2b8
a.karamiani <aziz.karamiani@gmail.com>
2023-02-27 23:19:06 +0330

    For Loop (Month Number)

---
+++ src/44- For Loop.kt
@@ -0,0 +1,22 @@
+package main
+
+fun main(args: Array<String>) {
+    for(i in 1..12) {
+        val month = when(i) {
+            1 -> "January"
+            2 -> "February"
+            3 -> "March"
+            4 -> "April"
+            5 -> "May"
+            6 -> "June"
+            7 -> "July"
+            8 -> "August"
+            9 -> "September"
+            10 -> "October"
+            11 -> "November"
+            12 -> "December"
+            else -> ""
+        }
+        println("Month #$i is called $month")
+    }
+}
