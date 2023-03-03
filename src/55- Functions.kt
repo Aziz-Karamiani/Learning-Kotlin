2548f3400b2b7a19a424ebad5f6d9fdf902b9199
a.karamiani <aziz.karamiani@gmail.com>
2023-03-03 09:40:21 +0330

    Functions Practice

--- src/55- Functions.kt
+++ src/55- Functions.kt
@@ -3,8 +3,18 @@ package main
 fun main(args: Array<String>) {
     for (i in 0 until 10)
         helloWorld()
+
+    for (i in 1..5)
+        multiplyNumber()
 }
 
 fun helloWorld() {
     println("Hello World.")
+}
+
+fun multiplyNumber() {
+    println("Please enter a number")
+    val usrInput = readLine() ?: ""
+    val number = usrInput.toInt()
+    println("$number * 17 = ${number * 17}")
 }
