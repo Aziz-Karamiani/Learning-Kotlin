da0b95fd6a63dfb0aeae13e9b0ec772dc2d3e19a
a.karamiani <aziz.karamiani@gmail.com>
2023-03-08 16:14:08 +0330

    User Play Tic Tok Toe

--- src/65- Tic Tok Toe.kt
+++ src/65- Tic Tok Toe.kt
@@ -10,7 +10,27 @@ fun main(args: Array<String>) {
         }
         board.add(row)
     }
-    printBoard()
+
+    var continueGame = true
+    do {
+        println("Please enter a position (eg. 1, 2)")
+        val userInput = readLine() ?: ""
+        var x = 0
+        var y = 0
+        try {
+            val position = userInput.split(",")
+            x = position[0].trim().toInt()
+            y = position[1].trim().toInt()
+            if (board[x - 1][y - 1] != "") {
+                println("This position already taken.")
+            } else {
+                board[x - 1][y - 1] = "X"
+                printBoard()
+            }
+        } catch (e: Exception) {
+            println("Invalid input, please try again.")
+        }
+    } while (continueGame)
 }
 
 fun printBoard() {
