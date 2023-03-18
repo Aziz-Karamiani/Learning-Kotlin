06e020e4db61222e90d6986b092f1297620c0fa7
a.karamiani <aziz.karamiani@gmail.com>
2023-03-19 00:19:05 +0330

    Type Casting + Lazy + Lateinit

---
+++ src/128- lazy.kt
@@ -0,0 +1,10 @@
+import kotlin.random.Random
+
+fun main(args: Array<String>) {
+    val someLargeVariable: String by lazy {
+        "Some large value"
+    }
+
+    if(Random.nextInt() % 2 == 0)
+        println(someLargeVariable)
+}
